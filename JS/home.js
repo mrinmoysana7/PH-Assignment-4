9de24572a9b1cfc8398b2532d8ffb85f6354ffca
mrinmoysana7 <mrinmoysana7@gmail.com>
2026-03-03 19:07:02 +0530

    95% done

--- JS/home.js
+++ JS/home.js
@@ -4,12 +4,13 @@ const tabInActive = ["bg-white", "text-slate-700", "border-slate-200", "text-bla
 
 const availableJobs = document.getElementById("jobs-container");
 const interviewTab = document.getElementById("interview-container");
-const rejectTab = document.getElementById("reject-container")
+const rejectTab = document.getElementById("reject-container");
+const noJob = document.getElementById("no-jobs-section");
 
 function switchTab (tab) {
     // console.log(tab)
     const tabs = ["all", "interview", "rejected"];
-
+    currentTab = tab;
     for(const t of tabs){
         const tabName = document.getElementById("tab-" + t);
         if(t === tab){
@@ -21,25 +22,39 @@ function switchTab (tab) {
 
         }
     }
+    noJob.classList.add("hidden");
 
     const pages = [availableJobs, interviewTab, rejectTab];
 
     for (const page of pages){
         page.classList.add("hidden");
     }
+    
 
     if(tab === "all") {
         availableJobs.classList.remove("hidden");
+        if(availableJobs.children.length < 1){
+            noJob.classList.remove("hidden");
+        }    
     } else if(tab === "interview") {
         interviewTab.classList.remove("hidden");
+        if(interviewTab.children.length < 1){
+            noJob.classList.remove("hidden");
+        }
     } else {
         rejectTab.classList.remove("hidden");
-    }
-}
+        if(rejectTab.children.length < 1){
+            noJob.classList.remove("hidden");
+        }
+    };
+    updateStatNumber();
+};
+
 
 const totalNum = document.getElementById("stat-total");
 const inrterviewNum = document.getElementById("stat-interview");
 const rejectNum = document.getElementById("stat-reject");
+const availJob = document.getElementById("Available-jobs");
 
 totalNum.innerText = availableJobs.children.length;
 
@@ -55,24 +70,36 @@ document.getElementById("jobs-container").addEventListener("click", function(eve
     if(targetClickEvent.classList.contains("interview")){
        status.innerText = "Interviewed";
        interviewTab.appendChild(card);
-       updateStatNumber()
     }
     if(targetClickEvent.classList.contains("rejected")){
        status.innerText = "Rejected";
        rejectTab.appendChild(card);
-       updateStatNumber()
     }
     if(targetClickEvent.classList.contains("delete")){
         parent.removeChild(card);
-        updateStatNumber()
-    }
-    console.log(parent);
+        
+    };
+    updateStatNumber();
 });
 
 function updateStatNumber() {
-   totalNum.innerText = availableJobs.children.length;
-   inrterviewNum.innerText = interviewTab.children.length;
-   rejectNum.innerText = rejectTab.children.length;
+const stats = {
+    all: availableJobs.children.length,
+    interview: interviewTab.children.length,
+    rejected: rejectTab.children.length  
 }
+totalNum.innerText = stats.all;
+inrterviewNum.innerText = stats.interview;
+rejectNum.innerText = stats.rejected;
+
+availJob.innerText = stats[currentTab];
+
+if(stats[currentTab] < 1){
+   noJob.classList.remove("hidden");
+} else {
+   noJob.classList.add("hidden");
+}
+}
+
 
 updateStatNumber();
